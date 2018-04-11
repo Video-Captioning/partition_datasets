2d79fcdd1834be561fec7319d0bd53246d84b0bb
Karl Edwards <edwards.karl@ymail.com>
2018-04-11 03:38:27 -0700

    Add files via upload

---
+++ show_test_sets.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# --- Configuration
+datafile=summary.csv # Name of input data file to visit in each subdirectory
+
+# --- Verify command-line parameters, giving usage instructions when warranted
+if (( $# != 1 ))
+then
+  echo 
+  echo "Usage:"
+  echo "  sh $0 keyword"
+  echo "Example:"
+  echo "  sh $0 disc"
+  exit 1
+fi
+
+# --- Get the keyword from the command-line
+keyword=$1
+
+# --- Find all partitions on keyword
+for i in `ls -d $keyword[0-9][0-9]/*/`; do
+  for j in `ls -d $i* | grep $datafile`; do
+    echo Batch='"'$i'"'
+  done
+done

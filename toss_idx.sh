cd57f30aa6173395d26cb9b2f883aded56dd14ef
Karl Edwards <edwards.karl@ymail.com>
2018-04-27 18:52:46 -0700

    Update toss_idx.sh

--- toss_idx.sh
+++ toss_idx.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
 
-# Get command-line arguments, giving usage instructions, when warranted
-if (( $# != 2 ))
-then
-  echo 
-  echo "Usage:"
-  echo "  sh $0 idx dat"
-  echo "Example:"
-  echo "  sh $0 idx labeled_3471.txt"
-  exit 1
-fi
+# Remove the specified image indexes, and keep the rest
 
-# Create a script to remove
-# selected rows, by row_id, from data
-sed 's/.*/&d/g' $1 > tmp.sed
-# Run the script
-sed -f tmp.sed $2
+TOSS_ROWS=$1
+CLASS_LABELS=$2
+
+sed 's/.*/&d/g' $TOSS_ROWS   >   script_to_remove_specified_rows_from
+sed -f script_to_remove_specified_rows_from   $CLASS_LABELS
